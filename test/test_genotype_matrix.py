bb2431f899efec53312f53d2e4bea61b3d8ed2fe
Maria Samblas <masamma2@inf.upv.es>
2015-11-10 13:00:43 +0100

    New functions to merge two vcf files or vcf file with an array snp file.

--- test/test_genotype_matrix.py
+++ test/test_genotype_matrix.py
@@ -9,10 +9,15 @@ import unittest
 from os.path import join
 from test.test_utils import TEST_DATA_DIR
 from variation.genotypes_matrix import (GenotypesMatrixParser,
-                                        IUPAC_CODING, STANDARD_GT)
+                                        IUPAC_CODING, STANDARD_GT,
+                                        change_gts_chain, decode_gts,
+                                        collapse_gts, encode_gts,
+                                        merge_sorted_variations, merge_snps,
+    merge_alleles)
 import os
-from variation.variations.vars_matrices import VariationsH5, put_vars_from_csv
+from variations.vars_matrices import VariationsH5, put_vars_from_csv
 import numpy
+from variation.genotypes_matrix import count_compatible_snps_in_chains
 
 
 class GtMatrixTest(unittest.TestCase):
@@ -30,7 +35,6 @@ class GtMatrixTest(unittest.TestCase):
         for x, y in zip(parser, expected):
             for key in x.keys():
                 assert x[key] == y[key]
-
         fpath = join(TEST_DATA_DIR, 'csv', 'standard_ex.tsv')
         parser = GenotypesMatrixParser(open(fpath), STANDARD_GT,
                                        2, sep='\t',
@@ -77,22 +81,204 @@ class GtMatrixTest(unittest.TestCase):
                                        snp_fieldnames=['chrom', 'pos'])
         out_fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex.h5')
         try:
-            os.remove(out_fpath + '.hdf5')
+            os.remove(out_fpath)
         except FileNotFoundError:
             pass
-        h5 = VariationsH5(out_fpath + '.hdf5', mode='w')
+        h5 = VariationsH5(out_fpath, mode='w')
         put_vars_from_csv(parser, h5, 100)
         exp = [b'SL2.40ch02',b'SL2.40ch02', b'SL2.40ch02']
         assert list(h5['/variations/chrom'][:]) == exp
         assert list(h5['/variations/ref'][:]) == [b'T', b'C', b'T']
-        assert list(h5['/variations/pos'][:]) == [b'331954', b'681961',
-                                                  b'1511764']
+        assert list(h5['/variations/pos'][:]) == [331954, 681961,
+                                                  1511764]
         exp = numpy.array([[[1, 1], [0, 0], [-1, -1]],
                            [[0, 0], [0, 0], [-1, -1]],
                            [[0, 0], [0, 0], [1, 0]]])
         assert numpy.all(h5['/calls/GT'][:] == exp)
 
+        fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex2.txt')
+        parser = GenotypesMatrixParser(open(fpath), IUPAC_CODING,
+                                       2, sep='\t',
+                                       snp_fieldnames=['chrom', 'pos'])
+        out_fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex2.h5')
+        try:
+            os.remove(out_fpath)
+        except FileNotFoundError:
+            pass
+        h5 = VariationsH5(out_fpath, mode='w')
+        put_vars_from_csv(parser, h5, 100)
+        print(list(h5['/variations/chrom'][:]))
+        print(list(h5['/variations/ref'][:]))
+        print(list(h5['/variations/pos'][:]))
+        print(h5['/calls/GT'][:])
+
+    def test_count_compatible_snsp_in_chains(self):
+        fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex.h5')
+        h5 = VariationsH5(fpath, "r")
+        chains_matrix = numpy.array([[[b'G', b'T']],
+                                     [[b'G', b'T']],
+                                     [[b'G', b'T']]])
+        result = count_compatible_snps_in_chains(h5, chains_matrix)
+        assert result == 1
+
+    def test_change_gts_chain(self):
+        fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex.h5')
+        h5 = VariationsH5(fpath, "r")
+        mask = numpy.array([True, False, True])
+        alleles = change_gts_chain(h5, mask)
+        assert numpy.all(alleles == numpy.array([[b'A', b'C', b''],
+                                                 [b'C', b'', b''],
+                                                 [b'A', b'T', b'']]))
+
+    def test_decode_gts(self):
+        gts = numpy.array([[[0, 0], [0, 1]], [[1, 1], [1, 0]]])
+        alleles = numpy.array([['A', 'T'], ['C', 'T']])
+        expected = numpy.array([[['A', 'A'], ['A', 'T']],
+                                [['T', 'T'], ['T', 'C']]])
+        assert numpy.all(decode_gts(alleles, gts) == expected)
+
+    def test_encode_gts(self):
+        gts = numpy.array([[['A', 'A'], ['A', 'T']],
+                           [['T', 'T'], ['T', 'C']]])
+        alleles = numpy.array([['A', 'T'], ['C', 'T']])
+        expected = numpy.array([[[0, 0], [0, 1]], [[1, 1], [1, 0]]])
+        assert numpy.all(encode_gts(gts, alleles) == expected)
+        gts = numpy.array([[['AA', 'AA'], ['G', 'T']]])
+        alleles = numpy.array([['AA', 'T', 'G']])
+        expected = numpy.array([[[0, 0], [2, 1]]])
+        assert numpy.all(encode_gts(gts, alleles) == expected)
+
+    def test_collapse_gts(self):
+        gts = numpy.array([[['A', 'A'], ['A', 'T']],
+                           [['T', 'T'], ['T', 'C']]])
+        base_allele = 'ATCAC'
+        relative_positions = [1, 4]
+        expected = numpy.array([[['AACAT', 'AACAT'],
+                                 ['AACAT', 'ATCAC']]])
+        result = collapse_gts(base_allele, gts, relative_positions)
+        print(result)
+        assert numpy.all(result == expected)
+
+    def test_merge_h5(self):
+        fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex.h5')
+        h5_1 = VariationsH5(fpath, "r")
+        fpath = join(TEST_DATA_DIR, 'csv', 'iupac_ex2.h5')
+        h5_2 = VariationsH5(fpath, "r")
+        expected = [[681961, 681961], [1511764, 1511764],
+                    [None, 15164], [None, 15184]]
+        for snp, exp in zip(merge_sorted_variations(h5_1, h5_2), expected):
+            for x, y in zip(snp, exp):
+                try:
+                    assert x['/variations/pos'][0] == y
+                except TypeError:
+                    assert x is None
+
+    def test_merge_alleles(self):
+        alleles1 = numpy.array(['AT', 'TT'])
+        alleles2 = numpy.array(['A', 'TT'])
+        exp = numpy.array(['AT', 'TT', 'A'])
+        assert numpy.all(merge_alleles(alleles1, alleles2) == exp)
+
+    def test_merge_snps(self):
+        class Variation(dict):
+            def set_samples(self, samples):
+                self.samples = samples
+
+        merged = {'/variations/chrom': numpy.array([['']], dtype='S10'),
+                  '/variations/pos': numpy.array([[-1]]),
+                  '/variations/ref': numpy.array([['']], dtype='S10'),
+                  '/variations/alt': numpy.array([['', '', '']], dtype='S10'),
+                  '/variations/qual': numpy.array([[-1, ]]),
+                  '/variations/info/AF': numpy.array([[-1, ]]),
+                  '/variations/info/DP': numpy.array([[-1,-1]]),
+                  '/calls/GT': numpy.array([[[-1, -1], [-1, -1], [-1, -1],
+                                             [-1, -1], [-1, -1]]]),
+                  '/calls/DP': numpy.array([[-1, -1, -1, -1, -1]])}
+        snp1 = {'/variations/chrom': numpy.array([['chr1']], dtype='S10'),
+                '/variations/pos': numpy.array([[10]]),
+                '/variations/ref': numpy.array([['AT']], dtype='S10'),
+                '/variations/alt': numpy.array([['TT', 'G']], dtype='S10'),
+                '/variations/qual': numpy.array([[120]]),
+                '/variations/info/AF': numpy.array([[15]]),
+                '/variations/info/DP': numpy.array([[200]]),
+                '/calls/GT': numpy.array([[[0, 0], [0, 1], [0, 2]]]),
+                '/calls/DP': numpy.array([[20, 30, 10]])}
+        snp1 = Variation(snp1)
+        snp1.set_samples(['s1', 's2', 's3'])
+        snp2 = {'/variations/chrom': numpy.array([['chr1']], dtype='S10'),
+                '/variations/pos': numpy.array([[11]]),
+                '/variations/ref': numpy.array([['A']], dtype='S10'),
+                '/variations/alt': numpy.array([['T']], dtype='S10'),
+                '/variations/info/AF': numpy.array([[17]]),
+                '/variations/info/DP': numpy.array([[210]]),
+                '/calls/GT': numpy.array([[[1, 1], [0, 1]]])}
+        snp2 = Variation(snp2)
+        snp2.set_samples(['s4', 's5'])
+        merge_snps(snp1, snp2, 0, merged,
+                   fields_funct={'/variations/info/AF': min})
+        expected = {'/variations/chrom': numpy.array([['chr1']], dtype='S10'),
+                    '/variations/pos': numpy.array([[10]]),
+                    '/variations/ref': numpy.array([['AT']], dtype='S10'),
+                    '/variations/alt': numpy.array([['TT', 'G', 'AA']], dtype='S10'),
+                    '/variations/qual': numpy.array([[120]]),
+                    '/variations/info/AF': numpy.array([[15]]),
+                    '/variations/info/DP': numpy.array([[200, 210]]),
+                    '/calls/GT': numpy.array([[[0, 0], [0, 1], [0, 2],
+                                               [0, 0], [3, 0]]]),
+                    '/calls/DP': numpy.array([[20, 30, 10, -1, -1]])}
+        for key in expected.keys():
+            assert numpy.all(merged[key] == expected[key])
+
+        merged = {'/variations/chrom': numpy.array([['']], dtype='S10'),
+                  '/variations/pos': numpy.array([[-1]]),
+                  '/variations/ref': numpy.array([['']], dtype='S10'),
+                  '/variations/alt': numpy.array([['', '']], dtype='S10'),
+                  '/variations/qual': numpy.array([[-1, ]]),
+                  '/variations/info/AF': numpy.array([[-1, ]]),
+                  '/variations/info/DP': numpy.array([[-1,-1]]),
+                  '/calls/GT': numpy.array([[[-1, -1], [-1, -1], [-1, -1],
+                                             [-1, -1], [-1, -1]]]),
+                  '/calls/DP': numpy.array([[-1, -1, -1, -1, -1]])}
+        merge_snps(snp1, None, 0, merged,
+                   fields_funct={'/variations/info/AF': min})
+        expected = {'/variations/chrom': numpy.array([['chr1']], dtype='S10'),
+                    '/variations/pos': numpy.array([[10]]),
+                    '/variations/ref': numpy.array([['AT']], dtype='S10'),
+                    '/variations/alt': numpy.array([['TT', 'G']], dtype='S10'),
+                    '/variations/qual': numpy.array([[120]]),
+                    '/variations/info/AF': numpy.array([[15]]),
+                    '/variations/info/DP': numpy.array([[200, -1]]),
+                    '/calls/GT': numpy.array([[[0, 0], [0, 1], [0, 2],
+                                               [-1, -1], [-1, -1]]]),
+                    '/calls/DP': numpy.array([[20, 30, 10, -1, -1]])}
+        for key in expected.keys():
+            assert numpy.all(merged[key] == expected[key])
+
+        merged = {'/variations/chrom': numpy.array([['']], dtype='S10'),
+                  '/variations/pos': numpy.array([[-1]]),
+                  '/variations/ref': numpy.array([['']], dtype='S10'),
+                  '/variations/alt': numpy.array([['', '']], dtype='S10'),
+                  '/variations/qual': numpy.array([[-1, ]]),
+                  '/variations/info/AF': numpy.array([[-1, ]]),
+                  '/variations/info/DP': numpy.array([[-1, -1]]),
+                  '/calls/GT': numpy.array([[[-1, -1], [-1, -1], [-1, -1],
+                                             [-1, -1], [-1, -1]]]),
+                  '/calls/DP': numpy.array([[-1, -1, -1, -1, -1]])}
+        merge_snps(None, snp2, 0, merged,
+                   fields_funct={'/variations/info/AF': min})
+        expected = {'/variations/chrom': numpy.array([['chr1']], dtype='S10'),
+                    '/variations/pos': numpy.array([[11]]),
+                    '/variations/ref': numpy.array([['A']], dtype='S10'),
+                    '/variations/alt': numpy.array([['T']], dtype='S10'),
+                    '/variations/info/AF': numpy.array([[17]]),
+                    '/variations/info/DP': numpy.array([[-1, 210]]),
+                    '/calls/GT': numpy.array([[[-1, -1], [-1, -1], [-1, -1],
+                                               [1, 1], [0, 1]]]),
+                    '/calls/DP': numpy.array([[-1, -1, -1, -1, -1]])}
+        for key in expected.keys():
+            assert numpy.all(merged[key] == expected[key])
+
 
 if __name__ == "__main__":
-#     import sys;sys.argv = ['', 'GtMatrixTest.test_put_vars_from_csv']
+    import sys;sys.argv = ['', 'GtMatrixTest.test_merge_snps']
     unittest.main()
--- variation/genotypes_matrix.py
+++ variation/genotypes_matrix.py
@@ -6,7 +6,11 @@
 # pylint: disable=C0111
 from csv import DictReader
 from itertools import chain
-from variation import MISSING_VALUES
+from variation import MISSING_VALUES, DEF_DSET_PARAMS
+from variation.variations.filters import filter_gt_no_data
+import numpy
+from variations.vars_matrices import VariationsH5
+from variation.variations.vars_matrices import _create_matrix
 
 
 IUPAC = {'A': 'AA', 'T': 'TT', 'C': 'CC', 'G': 'GG',
@@ -90,8 +94,13 @@ class GenotypesMatrixParser():
                     if alt_allele == gt[1]:
                         gt_new[1] = i + 1
             parsed_record['gts'].append(gt_new)
+
         if not parsed_record['alt']:
             parsed_record['alt'] = [MISSING_VALUES[str]]
+        try:
+            parsed_record['pos'] = int(parsed_record['pos'])
+        except KeyError:
+            pass
         return parsed_record
 
     def _create_reader(self):
@@ -111,3 +120,317 @@ class GenotypesMatrixParser():
             record = self._parse_record(record)
             if len(record['alt']) <= self.max_alt_allele:
                 yield record
+
+
+def count_compatible_snps_in_chains(variations, chains_matrix,
+                                    n_snps_check=10000):
+    variations = filter_gt_no_data(variations, n_snps_check)
+    ref = variations['/variations/ref'][:]
+    alt = variations['/variations/alt'][:]
+    alleles = numpy.append(ref.reshape(ref.shape[0], 1), alt, axis=1)
+    chain_counts = numpy.zeros(chains_matrix.shape[1])
+    for snp_alleles, chains_snp_alleles in zip(alleles, chains_matrix):
+        for i, chain_alleles in enumerate(chains_snp_alleles):
+            compatible = True
+            for allele in snp_alleles:
+                if allele not in chain_alleles and allele != b'':
+                    compatible = False
+                    break
+            if compatible:
+                chain_counts[i] += 1
+    return chain_counts
+
+
+def _rev_compl(seq):
+    complementary = {b'A': b'T', b'G': b'C', b'T': b'A', b'C': b'G', b'': b''}
+    return numpy.array([complementary[x] for x in seq])
+
+
+def change_gts_chain(variations, mask):
+    ref = variations['/variations/ref'][:]
+    alt = variations['/variations/alt'][:]
+    alleles = numpy.append(ref.reshape(ref.shape[0], 1), alt, axis=1)
+    alleles[mask] = numpy.apply_along_axis(_rev_compl, 0, alleles[mask])
+    return alleles
+
+
+def merge_matrices(mat1, mat2, rownames1, rownames2, colnames1, colnames2,
+                   dtype='int'):
+    pass
+
+
+def _max_length(chars_array):
+    return max([len(x) for x in chars_array])
+
+
+def decode_gts(alleles, gts):
+    decoded = gts.copy().astype('str')
+    for i, (snp_gts, snp_alleles) in enumerate(zip(gts, alleles)):
+        decoded[i] = snp_alleles[snp_gts]
+    return decoded
+
+
+def encode_gts(gts, alleles):
+    encoded = numpy.zeros(gts.shape)
+    i = 0
+    for j in range(alleles.shape[1]):
+        allele = alleles[:, j].reshape(alleles.shape[0], 1).T
+        if allele != b'':
+            encoded[gts == allele] = i
+            i += 1
+        else:
+            encoded[gts == allele] = MISSING_VALUES[int]
+    return encoded
+
+
+def collapse_gts(base_allele, gts, positions, max_allele_length=35):
+    collapsed_gts = gts[0].copy().reshape(1, gts.shape[1], gts.shape[2])
+    collapsed_gts = collapsed_gts.astype(numpy.dtype(('U', max_allele_length)))
+    for i in range(gts.shape[1]):
+        for j in range(gts.shape[2]):
+            allele = [x for x in base_allele.decode('utf-8')]
+            for k, pos in enumerate(positions):
+                allele[pos] = gts[k, i, j]
+            collapsed_gts[0, i, j] = ''.join(allele)
+    return collapsed_gts
+
+
+def merge_alleles(alleles_vcf, alleles_collapse):
+    for allele_collapse in alleles_collapse:
+        if allele_collapse in alleles_vcf:
+            continue
+        alleles_merged = numpy.append(alleles_vcf, allele_collapse)
+    return alleles_merged
+
+
+def create_variations(variations_1, variations_2, fpath):
+    variations_merged = VariationsH5(fpath, "w")
+    h5_paths = set(variations_1.keys()+variations_2.keys())
+    h5_samples = variations_1.samples+variations_2.samples
+    for path in h5_paths:
+        kwargs = DEF_DSET_PARAMS.copy()
+        kwargs['shape'] = variations_1[path].shape
+        kwargs['shape'][0] = (variations_1[path].shape[0] +
+                              variations_2[path].shape[0])
+        if 'calls' in path:
+            kwargs['shape'][1] = len(h5_samples)
+        kwargs['dtype'] = variations_1[path].dtype
+        kwargs['maxshape'] = variations_1[path].maxshape
+        if 'id' in path:
+            kwargs['maxshape'] = (None, None)
+        kwargs['chunks'] = variations_1[path].chunks
+        _create_matrix(variations_merged, path, **kwargs)
+    return variations_merged
+
+
+def are_overlapping(var1, var2):
+    pos1 = var1['/variations/pos']
+    pos2 = var2['/variations/pos']
+    return (var1['/variations/chrom'] == var2['/variations/chrom'] and
+            pos2 >= pos1 and pos2 < pos1 + len(var1['/variations/ref']))
+
+
+def merge_snps(snp1, snp2, i, merged, fields_funct={}):
+    if '/variations/qual' not in fields_funct:
+        fields_funct['/variations/qual'] = min
+    for path in merged.keys():
+        if 'calls' in path:
+            if 'GT' in path:
+                if snp1 is not None:
+                    gts1 = snp1['/calls/GT']
+                    pos1 = snp1['/variations/pos']
+                    ref1 = snp1['/variations/ref']
+                    alt1 = snp1['/variations/alt']
+                    alleles1 = numpy.append(ref1.reshape(ref1.shape[0], 1), alt1, axis=1)
+                    chrom1 = snp1['/variations/chrom']
+
+                if snp2 is not None:
+                    gts2 = snp2['/calls/GT']
+                    pos2 = snp2['/variations/pos']
+                    ref2 = snp2['/variations/ref']
+                    alt2 = snp2['/variations/alt']
+                    alleles2 = numpy.append(ref2.reshape(ref2.shape[0], 1), alt2,
+                                            axis=1)
+                    chrom2 = snp2['/variations/chrom']
+
+                if snp1 is None:
+                    merged['/variations/ref'][i] = ref2
+                    merged['/variations/alt'][i] = alt2
+                    merged['/variations/pos'][i] = pos2
+                    merged[path][i, -len(snp2.samples):, ] = gts2
+                    merged['/variations/chrom'][i] = chrom2
+                    continue
+                if snp2 is None:
+                    merged['/variations/ref'][i] = ref1
+                    merged['/variations/alt'][i] = alt1
+                    merged['/variations/pos'][i] = pos1
+                    merged[path][i, :len(snp1.samples), ] = gts1
+                    merged['/variations/chrom'][i] = chrom1
+                    continue
+
+                if chrom1 != chrom2:
+                    raise ValueError('Chromosome names must be equal')
+                merged['/variations/chrom'][i] = chrom1
+                if pos1 <= pos2:
+                    gts_decoded = decode_gts(alleles2, gts2)
+                    gts_collapsed = collapse_gts(alleles1[0, 0], gts_decoded,
+                                                 pos2 - pos1)
+                    alleles = numpy.array([x.decode('utf-8')
+                                           for x in alleles1[0]])
+                    alleles_merged = merge_alleles(alleles,
+                                                   numpy.unique(gts_collapsed))
+                    gts_encoded = encode_gts(gts_collapsed,
+                                             alleles_merged.reshape(1, alleles_merged.shape[0]))
+                    merged['/variations/ref'][i] = alleles_merged[0]
+                    merged['/variations/alt'][i] = alleles_merged[1:]
+                    merged['/variations/pos'][i] = pos1
+                    merged[path][i, :len(snp1.samples), ] = snp1[path][:]
+                    merged[path][i, -len(snp2.samples):, ] = gts_encoded
+
+                else:
+                    gts_decoded = decode_gts(alleles1, gts1)
+                    gts_collapsed = collapse_gts(alleles2[0, 0], gts_decoded,
+                                                 pos1 - pos2)
+                    alleles = numpy.array([x.decode('utf-8')
+                                           for x in alleles2[0]])
+                    alleles_merged = merge_alleles(alleles,
+                                                   numpy.unique(gts_collapsed))
+                    gts_encoded = encode_gts(gts_collapsed, alleles_merged)
+                    merged['/variations/ref'][i] = alleles_merged[0]
+                    merged['/variations/alt'][i] = alleles_merged[1:]
+                    merged['/variations/pos'][i] = pos2
+                    merged[path][i, :len(snp1.samples), ] = gts_encoded
+                    merged[path][i, -len(snp2.samples):, ] = snp2[path][:]
+
+            elif snp1 is not None:
+                try:
+                    merged[path][i, :len(snp1.samples), ] = snp1[path][:]
+                except KeyError:
+                    pass
+            elif snp2 is not None:
+                try:
+                    merged[path][i, -len(snp2.samples):, ] = snp2[path][:]
+                except KeyError:
+                    pass
+        else:
+            if 'ref' not in path and 'alt' not in path and 'pos' not in path and 'chrom' not in path:
+                missing = numpy.array(MISSING_VALUES[merged[path].dtype])
+                if path in fields_funct:
+                    # if one snp is None and the other has not a path
+                    try:
+                        try:
+                            try:
+                                qual = fields_funct[path](snp1[path], snp2[path])
+                                merged[path][i] = qual
+                            except (KeyError, TypeError):
+                                merged[path][i] = snp1[path]
+                        except (KeyError, TypeError):
+                            merged[path][i] = snp2[path]
+                    except KeyError:
+                        merged[path][i] = missing
+                else:
+                    try:
+                        try:
+                            try:
+                                merged[path][i, ] = numpy.append(snp1[path],
+                                                                 snp2[path])
+                            except (KeyError, TypeError):
+                                merged[path][i, ] = numpy.append(snp1[path],
+                                                                 missing)
+                        except (KeyError, TypeError):
+                            merged[path][i, ] = numpy.append(missing, snp2[path])
+                    except KeyError:
+                        merged[path][i, ] = numpy.append(missing, missing)
+
+
+def merge_sorted_variations(variations_1, variations_2):
+    snps_1 = iter(variations_1.iterate_chunks(chunk_size=1))
+    snps_2 = iter(variations_2.iterate_chunks(chunk_size=1))
+    snp_1 = next(snps_1)
+    snp_2 = next(snps_2)
+    stop = ""
+    are_snps1 = False
+    prev_chrom = None
+    while True:
+        pos1 = snp_1['/variations/pos']
+        pos2 = snp_2['/variations/pos']
+        if snp_1['/variations/chrom'] == snp_2['/variations/chrom']:
+            if are_overlapping(snp_1, snp_2) or are_overlapping(snp_2, snp_1):
+                result = snp_1, snp_2
+                try:
+                    snp_1 = next(snps_1)
+                except StopIteration:
+                    stop = snps_2
+                    yield result
+                    break
+                try:
+                    snp_2 = next(snps_2)
+                except StopIteration:
+                    stop = snps_1
+                    are_snps1 = True
+                    yield result
+                    break
+                overlap = False
+                try:
+                    while are_overlapping(result[0], snp_2):
+                        snp_2 = next(snps_2)
+                        overlap = True
+                except StopIteration:
+                    stop = snps_1
+                    are_snps1 = True
+                    break
+                try:
+                    while are_overlapping(result[1], snp_1):
+                        snp_1 = next(snps_1)
+                        overlap = True
+                except StopIteration:
+                    stop = snps_2
+                    break
+                if not overlap:
+                    yield result
+            elif pos1 < pos2:
+                yield snp_1, None
+                try:
+                    snp_1 = next(snps_1)
+                except StopIteration:
+                    stop = snps_2
+                    break
+            elif pos2 < pos1:
+                yield None, snp_2
+                try:
+                    snp_2 = next(snps_2)
+                except StopIteration:
+                    stop = snps_1
+                    are_snps1 = True
+                    break
+            prev_chrom = snp_1['/variations/chrom']
+        else:
+            if snp_1['/variations/chrom'] == prev_chrom:
+                yield snp_1, None
+                try:
+                    snp_1 = next(snps_1)
+                except StopIteration:
+                    stop = snps_2
+                    break
+            else:
+                yield None, snp_2
+                try:
+                    snp_2 = next(snps_2)
+                except StopIteration:
+                    stop = snps_1
+                    are_snps1 = True
+                    break
+    for snp in stop:
+        if are_snps1:
+            yield snp, None
+        else:
+            yield None, snp
+
+
+def merge_variations(variations1, variations2, merged_fpath, fields_funct):
+    merged_variations = create_variations(variations1, variations2,
+                                          merged_fpath)
+    for i, (snp1, snp2) in enumerate(merge_sorted_variations(variations1,
+                                                             variations2)):
+        merge_snps(snp1, snp2, i, merged_variations, fields_funct)
+    return merged_variations

f25ad91ccc23a72c5927da2fa67b2fee6002a998
Peio <pziarsolo@upv.es>
2015-11-25 12:52:41 +0100

    PeekableIterator added

---
+++ test/test_iterutils.py
@@ -0,0 +1,41 @@
+import unittest
+from variation.iterutils import PeekableIterator
+
+
+class PeekTest(unittest.TestCase):
+    def test_peekable_iter(self):
+        iterator = iter(['a', 'b', 'c', 'd', 'e', 'f'])
+        iterator = PeekableIterator(iterator)
+        assert 'a' == iterator.peek()
+        assert 'b' == iterator.peek()
+        assert 'a' == next(iterator)
+        assert 'c' == iterator.peek()
+        iterator.reset_peek()
+        assert 'b' == iterator.peek()
+        assert 'b' == next(iterator)
+
+        iterator = iter(['a', 'b', 'c', 'd', 'e', 'f'])
+        iterator = PeekableIterator(iterator)
+        iterator.peek()
+        iterator.peek()
+        iterator.peek()
+        iterator.reset_peek()
+        assert 'a' == iterator.peek()
+        iterator = iter(['a', 'b', 'c', 'd', 'e', 'f'])
+        iterator = PeekableIterator(iterator)
+        iterator.peek()
+        iterator.peek()
+        iterator.peek()
+        iterator.peek()
+        iterator.peek()
+        iterator.peek()
+
+        try:
+            iterator.peek()
+            self.fail('stopIteration expected')
+        except StopIteration:
+            pass
+        assert 'a' == next(iterator)
+
+if __name__ == '__main__':
+    unittest.main()

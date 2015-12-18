489606c08c42e98706871c06ab34db906798c738
Maria Samblas <masamma2@inf.upv.es>
2015-12-18 15:50:49 +0100

    cython improved vcf field parsers added

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-# from distutils.core import setup
-# from distutils.extension import Extension
-#
-# from Cython.Distutils import build_ext
-# from Cython.Build import cythonize
-# import numpy
-#
-# ext_modules = []
-#
-# # ext_modules += cythonize([Extension("variation.matrix.count_alleles",
-# #                                     sources=["variation/matrix/count_alleles.pyx"],
-# #                                     include_dirs=[numpy.get_include(), '.'])])
-#
-# cmdclass = {'build_ext': build_ext}
-# setup(
-#     # packages=['hola'],
-#     # package_dir=[],
-#     cmdclass=cmdclass,
-#     ext_modules=ext_modules,
-# )
+from distutils.core import setup
+from Cython.Build import cythonize
+
+
+setup(
+    packages=['variation.gt_parsers'],
+    ext_modules=cythonize("variation/gt_parsers/vcf_field_parsers.pyx"),
+)

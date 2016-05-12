fe4cb2c1e5433c07a3b08d86ee68850ec7b5f482
Jose Blanca <jblanca@upv.es>
2016-05-12 16:44:55 +0200

    working propotype of pipeline

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -12,20 +12,23 @@ from os.path import join
 
 from variation.variations.pipeline import Pipeline
 from variation.variations.filters import flt_hist_min_called_gts
-from variation.variations.vars_matrices import VariationsH5
+from variation.variations.vars_matrices import VariationsH5, VariationsArrays
 from test.test_utils import TEST_DATA_DIR
 
 
 class PipelineTest(unittest.TestCase):
-    def xtest_pipeline(self):
+    def test_pipeline(self):
         pipeline = Pipeline()
         hdf5 = VariationsH5(join(TEST_DATA_DIR, 'ril.hdf5'), mode='r')
-        pipeline.append(flt_hist_min_called_gts, kwargs={'min_': 0.1},
+        pipeline.append(flt_hist_min_called_gts, kwargs={'min_': 0.1,
+                                                         'range_': (0, 1)},
                         id_='filter1')
-        snps_out , res = pipeline.run(hdf5)
-        counts, edges = res.hists['filter1']
+        vars_out = VariationsArrays()
+        result = pipeline.run(hdf5, vars_out)
+        print(result)
 
-        # TODO test with arrarys
+        # TODO check same result with no pipeline
+        # TODO check with no range set
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'FilterTest']
--- variation/variations/pipeline.py
+++ variation/variations/pipeline.py
@@ -1,15 +1,14 @@
 
+import functools
+
+import numpy
+
 from variation import SNPS_PER_CHUNK
 
 
 class Pipeline():
-    def __init__(self, variations, chunk_size=SNPS_PER_CHUNK, kept_fields=None,
-                 ignored_fields=None):
-        self._steps = []
-        self._variations = variations
-        self.chunk_size = chunk_size
-        self.kept_fields = kept_fields
-        self.ignored_fields = ignored_fields
+    def __init__(self):
+        self._pipeline = []
 
     def append(self, funct, args=None, kwargs=None, id_=None):
         if args is None:
@@ -17,27 +16,60 @@ class Pipeline():
         if kwargs is None:
             kwargs = {}
         if id_ is None:
-            id_ = str(len(self._steps))
+            id_ = str(len(self._pipeline))
+        funct_name = funct.__name__
+        funct = functools.partial(funct, *args, **kwargs)
+        
+        step = {'funct': funct, 'id': id_, 'funct_name': funct_name}
+        self._pipeline.append(step)
 
-        funct = self._wrap_funct(funct)
-
-        step = {'funct': funct, 'args': args, 'kwargs': kwargs, 'id_': id_}
-        self._steps.append(step)
-
-    def _wrap_funct(self, funct):
-        ig_fields = self.ignored_fields
-
-        def process_slice(slic3):
-            chunk = self._variations.get_chunk(kept_fields=self.kept_fields,
-                                               ignored_fields=ig_fields)
-            return funct(chunk)
-        return funct
-
-    def run(self, snps_out=None):
-
-        slices = self.variations._create_iterate_chunk_slices(self.chunk_size)
-        reducer = list
-        for step in self._steps:
+    def _pipeline_funct(self, slic3, vars_in, vars_out, kept_fields=None,
+                        ignored_fields=None):
+        chunk = vars_in.get_chunk(slic3, kept_fields=kept_fields,
+                                  ignored_fields=ignored_fields)
+        results = []
+        for step in self._pipeline:
             funct = step['funct']
-            chunk_results = map(funct, slices)
-            reduce(reducer, chunk_results)
+            chunk, bins, edges = funct(chunk)
+            result = {'bins': bins,
+                      'edges': edges}
+            results.append(result)
+
+        vars_out.put_chunks([chunk])
+
+        return results
+
+    def _reduce_results(self, results):
+        result = {}
+        for slice_result in results:
+            for step_result, step in zip(slice_result, self._pipeline):
+                step_id = step['id']
+                if step_id not in result:
+                    result[step_id] = {'bins': None, 'edges': None,
+                                       'funct': step['funct_name']}
+                if result[step_id]['bins'] is None:
+                    result[step_id]['bins'] = step_result['bins']
+                    result[step_id]['edges'] = step_result['edges']
+                else:
+                    if not numpy.allclose(step_result['edges'],
+                                          result[step_id]['edges']):
+                        msg = 'The bin edges for a pipeline result in step %s '
+                        msg += 'funct %s do not match'
+                        msg %= step['id'], step['funct_name']
+                        raise RuntimeError(msg)
+                    result[step_id]['bins'] += step_result['bins']
+        return result
+
+    def run(self, vars_in, vars_out=None, chunk_size=SNPS_PER_CHUNK,
+            kept_fields=None, ignored_fields=None):
+
+        slices = vars_in._create_iterate_chunk_slices(chunk_size)
+        
+        pipeline_funct = functools.partial(self._pipeline_funct,
+                                           vars_in=vars_in,
+                                           vars_out=vars_out,
+                                           kept_fields=kept_fields,
+                                           ignored_fields=ignored_fields)
+        logs_and_hists = map(pipeline_funct, slices)
+        
+        return self._reduce_results(logs_and_hists)

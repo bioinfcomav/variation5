542d2b460eee2ce70a669925156d2561c824fbee
Jose Blanca <jblanca@upv.es>
2016-06-08 14:21:20 +0200

    test added to PCA

--- test/test_multivariate.py
+++ test/test_multivariate.py
@@ -2,10 +2,13 @@
 import unittest
 from os.path import join
 
+import numpy
+
 from test.test_utils import TEST_DATA_DIR
 from variation.variations.multivariate import (non_param_multi_dim_scaling,
                                                do_pcoa, do_pca)
-from variation.variations import VariationsH5
+from variation.variations.vars_matrices import VariationsArrays, VariationsH5
+from variation import GT_FIELD
 
 # Method could be a function
 # pylint: disable=R0201
@@ -22,11 +25,24 @@ class MultivariateTest(unittest.TestCase):
         assert res['stress'] < 0.1
         assert res['projections'].shape == (3, 3)
 
-        # print(do_pcoa(dists))
-
     def test_pca(self):
         hdf5 = VariationsH5(join(TEST_DATA_DIR, 'ril.hdf5'), mode='r')
         do_pca(hdf5)
+
+        varis = VariationsArrays()
+        gts = [[[0, 0], [0, 0], [1, 1]],
+               [[0, 0], [0, 0], [1, 1]],
+               [[0, 0], [0, 0], [1, 1]],
+               [[0, 0], [0, 0], [1, 1]],
+               ]
+        gts = numpy.array(gts)
+        varis[GT_FIELD] = gts
+        varis.samples = ['a', 'b', 'c']
+        res = do_pca(varis)
+        projs = res['projections']
+        assert projs.shape[0] == gts.shape[1]
+        assert numpy.allclose(projs[0], projs[1])
+        assert not numpy.allclose(projs[0], projs[2])
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'PlotTest.test_plot_boxplot_series']

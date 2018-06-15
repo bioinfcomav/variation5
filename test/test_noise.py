4db8af814628950746af15577555a7183c84193d
Jose Blanca <jblanca@upv.es>
2018-06-15 09:19:17 +0200

    gt set to missing interface modified

--- test/test_noise.py
+++ test/test_noise.py
@@ -14,7 +14,8 @@ import numpy
 
 from variation.variations.vars_matrices import VariationsArrays, VariationsH5
 from test.test_utils import TEST_DATA_DIR
-from variation.variations.random import set_gts_to_missing, sample_variations
+from variation.variations.random import (copy_setting_gts_to_missing,
+                                         sample_variations)
 from variation import GT_FIELD, MISSING_INT
 from variation.gt_parsers.vcf import VCFParser
 
@@ -22,21 +23,16 @@ from variation.gt_parsers.vcf import VCFParser
 class SetToMissingTest(unittest.TestCase):
 
     def test_set_to_missing(self):
-        vars = VariationsArrays()
-        hdf5 = VariationsH5(join(TEST_DATA_DIR, 'ril.hdf5'), mode='r')
-        hdf5.copy(vars)
+        orig_vars = VariationsH5(join(TEST_DATA_DIR, 'ril.hdf5'), mode='r')
 
-        orig_vars = vars.copy()
-        set_gts_to_missing(vars,
-                           percent_of_non_missing_gts_to_missing=90)
+        vars = copy_setting_gts_to_missing(orig_vars, gt_rate_to_missing=0.9)
 
-        orig_gts = orig_vars[GT_FIELD]
+        orig_gts = orig_vars[GT_FIELD][...]
         noise_gts = vars[GT_FIELD]
         assert orig_gts.shape == noise_gts.shape
         mask_different_gts = orig_gts != noise_gts
-        expected_num_gts_set_to_missing = int(round(numpy.sum(orig_gts != MISSING_INT) * 90 / 100))
+        expected_num_gts_set_to_missing = int(round(numpy.sum(orig_gts != MISSING_INT) * 0.9))
         assert expected_num_gts_set_to_missing == mask_different_gts.sum()
-
         assert not numpy.sum(orig_gts[mask_different_gts] == MISSING_INT)
 
         vcf_fhand = open(join(TEST_DATA_DIR, 'format_def.vcf'), 'rb')
@@ -58,9 +54,8 @@ class SetToMissingTest(unittest.TestCase):
                                     [[-1, -1], [-1, 2], [-1, -1]]])
         del snps[GT_FIELD]
         snps[GT_FIELD] = gts
-        vars = snps.copy()
-        set_gts_to_missing(vars,
-                           percent_of_non_missing_gts_to_missing=50)
+        vars = copy_setting_gts_to_missing(snps,
+                                           gt_rate_to_missing=0.5)
 
         noise_gts = vars[GT_FIELD]
         assert numpy.all(noise_gts == expected_gts)

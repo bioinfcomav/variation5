11df7c7a8ebba54cb0913a6c039aa6e404114f82
peio <pziarsolo@upv.es>
2019-10-03 16:58:36 +0200

    update setup to include needed numpy includes

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
+import numpy
+
 from Cython.Build import cythonize
 
-
 CYTHON_MODULES = ["variation/gt_parsers/vcf_field_parsers.pyx"]
-                 #  "variation/gt_writers/vcf_field_writer.pyx"
+                    # "variation/gt_writers/vcf_field_writer.pyx"
 
 setup(
     packages=['variation.gt_parsers', 'variation.variations',
               'variation.matrix', 'variation.utils', 'variation.gt_writers'],
     ext_modules=cythonize(CYTHON_MODULES),
+    include_dirs=[numpy.get_include()]
 )

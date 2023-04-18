17b10c39c6b1c651f3792b6770193f099ed039be
Jose Blanca <jblanca@upv.es>
2023-04-18 11:52:59 +0200

    pep8

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ import numpy
 from Cython.Build import cythonize
 
 CYTHON_MODULES = ["variation/gt_parsers/vcf_field_parsers.pyx"]
-                    # "variation/gt_writers/vcf_field_writer.pyx"
+# "variation/gt_writers/vcf_field_writer.pyx"
 
 setup(
-    packages=['variation.gt_parsers', 'variation.variations',
-              'variation.matrix', 'variation.utils', 'variation.gt_writers'],
+    packages=[
+        "variation.gt_parsers",
+        "variation.variations",
+        "variation.matrix",
+        "variation.utils",
+        "variation.gt_writers",
+    ],
     ext_modules=cythonize(CYTHON_MODULES),
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include()],
 )
